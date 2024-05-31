a4e659114e7ad9f934085f7310ba60e780ebd1bd
RichardSP <r.sandersonpope@codat.io>
2024-05-31 15:09:08 +0100

    Improved Result type

--- CommonUnions/Result.cs
+++ CommonUnions/Result.cs
@@ -7,20 +7,53 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static Scifa.UnionTypes.Result;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Scifa.UnionTypes;
 
 
 public static class Result
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="UntypedOk{T}"/> with the specified value.
+    /// This can be implictly cast to <see cref="Result{T, TError}"/> for any error type.
+    /// </summary>
+    /// <typeparam name="T">The type of <paramref name="value"/></typeparam>
+    /// <param name="value">The value</param>
+    /// <returns>An object that can be implicitly cast to an instance of <see cref="Result{T, TError}"/> regardless of the error type.</returns>
     public static UntypedOk<T> Ok<T>(T value) => new(value);
 
+    /// <summary>
+    /// Creates a new instance of <see cref="UntypedOk{T}"/> with the specified value.
+    /// This can be implictly cast to <see cref="Result{T, TError}"/> for any value type.
+    /// </summary>
+    /// <typeparam name="TError">The type of <paramref name="error"/></typeparam>
+    /// <param name="error">Details of the error that occurred</param>
+    /// <returns>An object that can be implicitly cast to an instance of <see cref="Result{T, TError}"/> regardless of the value type.</returns>
     public static UntypedError<TError> Error<TError>(TError error) => new(error);
 
+    /// <summary>
+    /// Execute a given function catching any exceptions that occur and returning them as an <see cref="Result{T, TError}"/> with the error type
+    /// set to <see cref="ExceptionDispatchInfo"/>. It is expected that you use <see cref="Result{T, TError}.MapError{TError2}(Func{TError, TError2})"/>
+    /// or equivalent to convert the <see cref="ExceptionDispatchInfo"/> into an alternative error type
+    /// </summary>
+    /// <typeparam name="T">The return type of <paramref name="action"/></typeparam>
+    /// <param name="action">The function to execute</param>
+    /// <returns>An instance of <see cref="Result{T, TError}"/> containing either the result of <paramref name="action"/> or an instance of
+    /// <see cref="ExceptionDispatchInfo"/> wrapping the exception thrown from <paramref name="action"/>.</returns>
     public static Result<T, ExceptionDispatchInfo> Catch<T>(Func<T> action)
         => Catch(action, _ => true);
 
+    /// <summary>
+    /// Execute a given function catching any exceptions that occur and returning them as an <see cref="Result{T, TError}"/> with the error type
+    /// set to <see cref="ExceptionDispatchInfo"/>. It is expected that you use <see cref="Result{T, TError}.MapError{TError2}(Func{TError, TError2})"/>
+    /// or equivalent to convert the <see cref="ExceptionDispatchInfo"/> into an alternative error type
+    /// </summary>
+    /// <typeparam name="T">The return type of <paramref name="action"/></typeparam>
+    /// <param name="action">The function to execute</param>
+    /// <param name="filter">A filter which limits the types of exceptions that will be caught and converted to <see cref="Result{T, TError}"/>.
+    /// Any exception _not_ matching this filter will remain uncaugt and will propagate to the caller</param>
+    /// <returns>An instance of <see cref="Result{T, TError}"/> containing either the result of <paramref name="action"/> or an instance of 
+    /// <see cref="ExceptionDispatchInfo"/> wrapping the exception thrown from <paramref name="action"/>.</returns>
     public static Result<T, ExceptionDispatchInfo> Catch<T>(Func<T> action, Predicate<Exception> filter)
     {
         T result;
@@ -36,9 +69,29 @@ public static class Result
         return Ok(result);
     }
 
+    /// <summary>
+    /// Execute a given function catching any exceptions that occur and returning them as an <see cref="Result{T, TError}"/> with the error type
+    /// set to <see cref="ExceptionDispatchInfo"/>. It is expected that you use <see cref="Result{T, TError}.MapError{TError2}(Func{TError, TError2})"/>
+    /// or equivalent to convert the <see cref="ExceptionDispatchInfo"/> into an alternative error type
+    /// </summary>
+    /// <typeparam name="T">The return type of <paramref name="action"/></typeparam>
+    /// <param name="action">The function to execute</param>
+    /// <returns>A task containing an instance of <see cref="Result{T, TError}"/> containing either the result of <paramref name="action"/> or an instance of
+    /// <see cref="ExceptionDispatchInfo"/> wrapping the exception thrown from <paramref name="action"/>.</returns>
     public static Task<Result<T, ExceptionDispatchInfo>> CatchAsync<T>(Func<Task<T>> action)
         => CatchAsync(action, _ => true);
 
+    /// <summary>
+    /// Execute a given function catching any exceptions that occur and returning them as an <see cref="Result{T, TError}"/> with the error type
+    /// set to <see cref="ExceptionDispatchInfo"/>. It is expected that you use <see cref="Result{T, TError}.MapError{TError2}(Func{TError, TError2})"/>
+    /// or equivalent to convert the <see cref="ExceptionDispatchInfo"/> into an alternative error type
+    /// </summary>
+    /// <typeparam name="T">The return type of <paramref name="action"/></typeparam>
+    /// <param name="action">The function to execute</param>
+    /// <param name="filter">A filter which limits the types of exceptions that will be caught and converted to <see cref="Result{T, TError}"/>.
+    /// Any exception _not_ matching this filter will remain uncaugt and will propagate to the caller</param>
+    /// <returns>A task containing an instance of <see cref="Result{T, TError}"/> containing either the result of <paramref name="action"/> or an instance of 
+    /// <see cref="ExceptionDispatchInfo"/> wrapping the exception thrown from <paramref name="action"/>.</returns>
     public static async Task<Result<T, ExceptionDispatchInfo>> CatchAsync<T>(Func<Task<T>> action, Predicate<Exception> filter)
     {
         T result;
@@ -54,20 +107,30 @@ public static class Result
         return Ok(result);
     }
 
-    public static UntypedOk<Unit> Ok() => new(Unit.Instance);
-
-    public static UntypedError<Unit> Error() => new(Unit.Instance);
-
-    public static Result<Unit, TError> IgnoreValue<T, TError>(this Result<T, TError> @this) => @this.Map(_ => Unit.Instance);
-
-    internal static Result<T[], TError> Collect<T, TError>(this IEnumerable<Result<T, TError>> results)
+    /// <summary>
+    /// Convert an enumerable of results into a single result containing an array of all the values or the first error that occurred
+    /// </summary>
+    /// <typeparam name="T">The type of the value contained in the <see cref="Result{T, TError}"/> instances.</typeparam>
+    /// <typeparam name="TError">The type of errors that may be contained in the <see cref="Result{T, TError}"/> instances.</typeparam>
+    /// <param name="results">The collection of results</param>
+    /// <returns>A single result containing all the values of the reuslts or the first error result if any failed.</returns>
+    public static Result<T[], TError> Collect<T, TError>(this IEnumerable<Result<T, TError>> results)
         => results.Aggregate(
             Result<IEnumerable<T>, TError>.Ok([]),
             (acc, result) => acc.Bind(accValue => result.Map(value => accValue.Append(value))),
             acc => acc.Map(values => values.ToArray())
         );
 
+    /// <summary>
+    /// A utility type that permits create an instance of <see cref="Result{T, TError}"/> without specifying the type of the error.
+    /// </summary>
     public record class UntypedOk<T>(T Value);
+
+    /// <summary>
+    /// A utility type that permits create an instance of <see cref="Result{T, TError}"/> without specifying the type of the value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Value"></param>
     public record class UntypedError<T>(T Value);
 }
 
