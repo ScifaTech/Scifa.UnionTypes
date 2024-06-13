5016377d94f31a2c3b5514be293961fdb0844bed
RichardSP <r.sandersonpope@codat.io>
2024-06-13 15:54:48 +0100

    Add Option.To/FromNullable

--- CommonUnions/Option.cs
+++ CommonUnions/Option.cs
@@ -17,13 +17,15 @@ public readonly partial struct Option<T>
     /// </summary>
     public T DefaultValue(Func<T> defaultValueFactory) => Match(some: x => x, none: defaultValueFactory);
 
+    public T? ToNullable() => Match(some: x => x, none: () => default);
+
     /// <summary>
     /// Implicitly convert an <see cref="UntypedNone"/> to an <see cref="Option{T}"/>.
     /// </summary>
     public static implicit operator Option<T>(UntypedNone _) => Option<T>.None();
 
     /// <summary>
-    /// Implicitly convert a value to an Option
+    /// Implicitly convert a value to <c>Option.Some(value)</c>. If <see langword="null" /> is provided, the result will be <c>Option.Some(null)</c>.
     /// </summary>
     public static implicit operator Option<T>(T value) => Option<T>.Some(value);
 }
@@ -33,6 +35,8 @@ public static class Option
     public static Option<T> Some<T>(T value) => Option<T>.Some(value);
     public static UntypedNone None => new UntypedNone();
 
+    public static Option<T> FromNullable<T>(T? value) where T : struct => value.HasValue ? Some(value.Value) : None;
+    public static Option<T> FromNullable<T>(T? value) where T : class => value is T ? Some(valu) : None;
 
     public static Option<U> Map<T, U>(this Option<T> @this, Func<T, U> map) => @this.Bind(x => Option<U>.Some(map(x)));
     public static Option<U> Bind<T, U>(this Option<T> @this, Func<T, Option<U>> map) => @this.Match(some: x => map(x), none: Option<U>.None);
